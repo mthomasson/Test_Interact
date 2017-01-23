8f575bae745ecfa72866b320e5c07354c336fcb9
mthomasson <thomassonmaxime@gmail.com>
2017-01-23 19:36:03 +0000

    logout corrected (adding request to server) infinite scroll on contact half ok, (buggy, the list is way longer than then number of items)

--- ContactList/js/controllers/ContactsController.js
+++ ContactList/js/controllers/ContactsController.js
@@ -1,68 +1,121 @@
 angular.module('contactsApp').controller('contactsCtrl',contactsCrtFnt);
 
-contactsCrtFnt.$inject=['$scope','$log','$window', '$sessionStorage', '$timeout', 'contacts'];
+contactsCrtFnt.$inject=['$scope','$log','$window', '$sessionStorage', 'contacts'];
 
-function contactsCrtFnt($scope, $log, $window, $sessionStorage, $timeout, contacts){
+function contactsCrtFnt($scope, $log, $window, $sessionStorage, contacts){
 
     //$log.info("in contact controller");
-
-    $scope.items = [];
-
     $sessionStorage.sync;
+    var nbContacts = 5;
 
     try {
         $scope.username = $sessionStorage.user['firstName'] + ' ' + $sessionStorage.user['lastName'];
-        var cont = contacts.GetContacts($sessionStorage.token['authToken']);
-        cont.then(
-            function (payload) {
-                $log.info(payload);
-                for (var i = 0; i < payload.data.length; i += 1) {
-                    var JSONToAddToItem = {};
-                    if (payload.data[i].displayName != null) {
-                        JSONToAddToItem["name"] = payload.data[i].displayName;
-                        if (payload.data[i].emails != null) {
-                            if (payload.data[i].emails.length > 0) {
-                                var email = "";
-                                for (var j = 0; j < payload.data[i].emails.length; j += 1) {
-                                    email += payload.data[i].emails[j].email;
-                                    if (j < payload.data[i].emails.length - 1) {
-                                        email += " / ";
-                                    }
-                                }
-                                JSONToAddToItem['emails'] = email;
-                            }
-                        }
-                        if (payload.data[i].phoneNumbers != null) {
-                            if (payload.data[i].phoneNumbers.length > 0) {
-                                var phones = "";
-                                for (var j = 0; j < payload.data[i].phoneNumbers.length; j += 1) {
-                                    phones += payload.data[i].phoneNumbers[j].number;
-                                    if (j < payload.data[i].phoneNumbers.length - 1) {
-                                        phones += " / ";
-                                    }
-                                }
-                                JSONToAddToItem['phoneNumbers'] = phones;
-                            }
-                        }
-                        $scope.items.push(JSONToAddToItem);
-                    }
-                }
-            },
-            function (errorPayload) {
-                $log.error(errorPayload);
-
-            }
-        );
-    } catch (e){
+    }catch (e){
         $log.info(e);
         $window.location.href = '../Login/index.html';
     }
 
+
+    $scope.items = {
+        numLoaded_: 0,
+        toLoad_: 0,
+        itemList:[],
+
+        getItemAtIndex: function(index) {
+            if (index > this.numLoaded_ && index<=nbContacts) {
+                    this.fetchMoreItems_(index);
+                return null;
+            }
+
+            return this.itemList[index];
+        },
+        getLength: function() {
+            return this.numLoaded_ + 10;
+        },
+        fetchMoreItems_: function(index) {
+            // For demo purposes, we simulate loading more items with a timed
+            // promise. In real code, this function would likely contain an
+            // $http request.
+
+            $log.info("this.numLoaded_ : ", this.numLoaded_);
+            $log.info("this.toLoad_ : ", this.toLoad_);
+            $log.info("nbContacts : ", nbContacts);
+            $log.info("index : ", index);
+            if (this.toLoad_ < index && index<=nbContacts) {
+                if (this.numLoaded_ != nbContacts) {
+                    if(this.toLoad_ + 20 < nbContacts) {
+                        this.toLoad_ += 20;
+                    }
+                    else{
+                        this.toLoad_ = nbContacts;
+                    }
+                    try {
+                        contacts.GetContacts($sessionStorage.token['authToken'], this.numLoaded_, this.toLoad_)
+                            .then(angular.bind(this, function (payload) {
+                                this.numLoaded_ += payload.data.length;
+                                $log.info(payload);
+
+                                nbContacts = payload["count"];
+                                for (var i = 0; i < payload.data.length; i += 1) {
+                                    var JSONToAddToItem = {};
+                                    if (payload.data[i].displayName != null) {
+                                        JSONToAddToItem["name"] = payload.data[i].displayName;
+                                        if (payload.data[i].emails != null) {
+                                            if (payload.data[i].emails.length > 0) {
+                                                var email = "";
+                                                for (var j = 0; j < payload.data[i].emails.length; j += 1) {
+                                                    email += payload.data[i].emails[j].email;
+                                                    if (j < payload.data[i].emails.length - 1) {
+                                                        email += " / ";
+                                                    }
+                                                }
+                                                JSONToAddToItem['emails'] = email;
+                                            }
+                                        }
+                                        if (payload.data[i].phoneNumbers != null) {
+                                            if (payload.data[i].phoneNumbers.length > 0) {
+                                                var phones = "";
+                                                for (var j = 0; j < payload.data[i].phoneNumbers.length; j += 1) {
+                                                    phones += payload.data[i].phoneNumbers[j].number;
+                                                    if (j < payload.data[i].phoneNumbers.length - 1) {
+                                                        phones += " / ";
+                                                    }
+                                                }
+                                                JSONToAddToItem['phoneNumbers'] = phones;
+                                            }
+                                        }
+                                        if (JSONToAddToItem != {}) {
+                                            this.itemList.push(JSONToAddToItem);
+                                        }
+                                    }
+                                }
+                            }));
+                    } catch (e) {
+                        $log.info(e);
+                        $window.location.href = '../Login/index.html';
+                    }
+                }
+                else{
+                    this.toLoad_ = this.numLoaded_;
+                }
+            }
+        }
+
+    };
+
+
+
     $scope.logout = function () {
         $log.info("logging out");
-        $sessionStorage.$reset();
+        var logout = contacts.Logout($sessionStorage.token['authToken']);
+        logout.then(function() {
+            $sessionStorage.$reset();
+            $window.location.href = '../Login/index.html';
+        });
+
+        //TODO ADD LOGOUTSERVICE TO API !!!!!!!!!!!!!!!!!!
         //$http.defaults.headers.common['authToken'] = "";
-        $window.location.href = '../Login/index.html';
+
 
     }
     
--- ContactList/js/services/ContactsService.js
+++ ContactList/js/services/ContactsService.js
@@ -5,17 +5,20 @@ contactsFnc.$inject=['$http','$q', '$log'];
 function contactsFnc($http, $q, $log) {
 	
 	var fncContainer={
-        GetContacts:GetContacts
+        GetContacts:GetContacts,
+        Logout:Logout
 	};
 
-	function GetContacts(token){
+	function GetContacts(token, offset, limit){
 		var deferred = $q.defer();
 
         $http.defaults.headers.common['authToken'] = token;
 
         $log.info($http.defaults.headers);
-        var URL_GetContacts = "https://internal-api-staging-lb.interact.io/v2/contacts";
-		$http.get(URL_GetContacts)
+
+        var URL_GetContacts = "https://internal-api-staging-lb.interact.io/v2/contacts?offset=" + offset + "&limit=" + limit;
+		$log.info(URL_GetContacts);
+        $http.get(URL_GetContacts)
    			.then(function successCallback(response) {
     				return deferred.resolve(response.data);
   				}, function errorCallback(response) {
@@ -23,6 +26,27 @@ function contactsFnc($http, $q, $log) {
   			});
 		
 		return deferred.promise;
+
+    }
+    function Logout(token){
+        var deferred = $q.defer();
+
+        //$http.defaults.headers.common['authToken'] = token;
+
+		body = {
+            "authToken":token
+        }
+        //$log.info($http.defaults.headers);
+
+        var URL_Logout = "https://internal-api-staging-lb.interact.io/v2/logout";
+        $http.post(URL_Logout, body)
+            .then(function successCallback(response) {
+                return deferred.resolve(response.data);
+            }, function errorCallback(response) {
+                return deferred.reject(response.data);
+            });
+
+        return deferred.promise;
 
     }
     return fncContainer;
